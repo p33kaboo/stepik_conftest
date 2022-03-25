347c91af65dfd06896d02a7e46af76dfc3c29937
Tim Ivanov <80482121+p33kaboo@users.noreply.github.com>
2022-03-25 10:20:21 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 class TestLang:
-    def test_star_check(self, browser):
+    def test_items(self, browser):
         browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
         browser.implicitly_wait(5)
         check_btn = browser.find_element(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
